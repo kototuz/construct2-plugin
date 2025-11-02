f5fd7b39899a4604f57bc8a886c7618b7156972c
kototuz <kototuzz@gmail.com>
2025-11-02 12:08:01 +0300

    Initial commit

---
+++ edittime.js
@@ -0,0 +1,145 @@
+﻿function GetPluginSettings()
+{
+    return {
+        "name":        "YandexSDK",
+        "id":          "YandexSDK",
+        "version":     "0.1",
+        "description": "YandexSDK API",
+        "author":      "Me",
+        "help url":    "http://www.scirra.com/manual/111/button",
+        "category":    "Web",
+        "type":        "object",
+        "dependency":  "sdk.js;sdk_init.js",
+        "rotatable":   false,
+        "flags":       pf_singleglobal
+    };
+};
+
+////////////////////////////////////////
+// Parameter types:
+// AddNumberParam(label, description [, initial_string = "0"])            // a number
+// AddStringParam(label, description [, initial_string = "\"\""])        // a string
+// AddAnyTypeParam(label, description [, initial_string = "0"])            // accepts either a number or string
+// AddCmpParam(label, description)                                        // combo with equal, not equal, less, etc.
+// AddComboParamOption(text)                                            // (repeat before "AddComboParam" to add combo items)
+// AddComboParam(label, description [, initial_selection = 0])            // a dropdown list parameter
+// AddObjectParam(label, description)                                    // a button to click and pick an object type
+// AddLayerParam(label, description)                                    // accepts either a layer number or name (string)
+// AddLayoutParam(label, description)                                    // a dropdown list with all project layouts
+// AddKeybParam(label, description)                                        // a button to click and press a key (returns a VK)
+// AddAnimationParam(label, description)                                // a string intended to specify an animation name
+// AddAudioFileParam(label, description)                                // a dropdown list with all imported project audio files
+
+////////////////////////////////////////
+    // Conditions
+
+    // AddCondition(id,                    // any positive integer to uniquely identify this condition
+    //                flags,                // (see docs) cf_none, cf_trigger, cf_fake_trigger, cf_static, cf_not_invertible,
+    //                                    // cf_deprecated, cf_incompatible_with_triggers, cf_looping
+    //                list_name,            // appears in event wizard list
+    //                category,            // category in event wizard list
+    //                display_str,        // as appears in event sheet - use {0}, {1} for parameters and also <b></b>, <i></i>
+    //                description,        // appears in event wizard dialog when selected
+    //                script_name);        // corresponding runtime function name
+//
+//     AddCondition(0, cf_trigger, "On clicked", "Button", "On clicked", "Triggered when the button is clicked.", "OnClicked");
+//
+// AddCondition(1, cf_none, "Is checked", "Button", "Is checked", "True if the checkbox is checked.", "IsChecked");
+
+////////////////////////////////////////
+// Actions
+
+// AddAction(id,                // any positive integer to uniquely identify this action
+//             flags,                // (see docs) af_none, af_deprecated
+//             list_name,            // appears in event wizard list
+//             category,            // category in event wizard list
+//             display_str,        // as appears in event sheet - use {0}, {1} for parameters and also <b></b>, <i></i>
+//             description,        // appears in event wizard dialog when selected
+//             script_name);        // corresponding runtime function name
+
+// AddStringParam("Text", "The text to set in the button.");
+// AddAction(0, af_none, "Set text", "Button", "Set text to {0}", "Set the button's text.", "SetText");
+//
+// AddStringParam("Tooltip", "The tooltip to set on the button.");
+// AddAction(1, af_none, "Set tooltip", "Button", "Set tooltip to {0}", "Set the button's tooltip.", "SetTooltip");
+//
+// AddComboParamOption("Invisible");
+// AddComboParamOption("Visible");
+// AddComboParam("Visibility", "Choose whether to hide or show the button.");
+// AddAction(2, af_none, "Set visible", "Appearance", "Set <b>{0}</b>", "Hide or show the button.", "SetVisible");
+//
+// AddComboParamOption("Disabled");
+// AddComboParamOption("Enabled");
+// AddComboParam("Mode", "Choose whether to enable or disable the button.");
+// AddAction(3, af_none, "Set enabled", "Button", "Set <b>{0}</b>", "Disable or enable the button.", "SetEnabled");
+//
+// AddAction(4, af_none, "Set focused", "Button", "Set focused", "Set the input focus to the button.", "SetFocus");
+//
+// AddStringParam("Property name", "A CSS property name to set on the control.", "\"color\"");
+// AddStringParam("Value", "A string to assign as the value for this CSS property.", "\"red\"");
+// AddAction(5, af_none, "Set CSS style", "Appearance", "Set CSS style {0} to {1}", "Set a CSS style on the control.", "SetCSSStyle");
+//
+// AddComboParamOption("Unchecked");
+// AddComboParamOption("Checked");
+// AddComboParam("State", "Choose the checkbox state.");
+// AddAction(6, af_none, "Set checked", "Button", "Set <b>{0}</b>", "Check or uncheck a checkbox.", "SetChecked");
+//
+// AddAction(7, af_none, "Toggle checked", "Button", "Toggle checked", "Toggle a checkbox.", "ToggleChecked");
+//
+// AddAction(8, af_none, "Set unfocused", "Button", "Set unfocused", "Remove the input focus from the button.", "SetBlur");
+
+////////////////////////////////////////
+// Expressions
+
+// AddExpression(id,            // any positive integer to uniquely identify this expression
+//                 flags,            // (see docs) ef_none, ef_deprecated, ef_return_number, ef_return_string,
+//                                // ef_return_any, ef_variadic_parameters (one return flag must be specified)
+//                 list_name,        // currently ignored, but set as if appeared in event wizard
+//                 category,        // category in expressions panel
+//                 exp_name,        // the expression name after the dot, e.g. "foo" for "myobject.foo" - also the runtime function name
+//                 description);    // description in expressions panel
+
+AddAction(0, 0, "Show Ads", "", "Ads", "Show Advertisment", "ShowAds");
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+];
+
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+    return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+    assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+    return new IDEInstance(instance);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+    assert2(this instanceof arguments.callee, "Constructor called as a function");
+
+    // Save the constructor parameters
+    this.instance = instance;
+    this.type = type;
+
+    // Set the default property values from the property table
+    this.properties = {};
+
+    for (var i = 0; i < property_list.length; i++)
+        this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+IDEInstance.prototype.OnCreate = function()
+{
+}
---
+++ runtime.js
@@ -0,0 +1,60 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.YandexSDK = function(runtime)
+{
+    this.runtime = runtime;
+};
+
+(function ()
+    {
+        /////////////////////////////////////
+        var pluginProto = cr.plugins_.YandexSDK.prototype;
+
+        /////////////////////////////////////
+        // Object type class
+        pluginProto.Type = function(plugin)
+        {
+            this.plugin = plugin;
+            this.runtime = plugin.runtime;
+        };
+
+        var typeProto = pluginProto.Type.prototype;
+
+        // called on startup for each object type
+        typeProto.onCreate = function()
+        {
+        };
+
+        /////////////////////////////////////
+        // Instance class
+        pluginProto.Instance = function(type)
+        {
+            this.type = type;
+            this.runtime = type.runtime;
+        };
+
+        var instanceProto = pluginProto.Instance.prototype;
+
+        // called whenever an instance is created
+        instanceProto.onCreate = function()
+        {
+        };
+
+        instanceProto.onDestroy = function ()
+        {
+        };
+
+        function Acts(){;}
+        Acts.prototype.ShowAds = function()
+        {
+            window.ysdk.adv.showFullscreenAdv();
+        };
+
+        pluginProto.acts = new Acts();
+    }());
